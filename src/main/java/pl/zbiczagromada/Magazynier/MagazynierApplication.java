04fd5f762d18f6f29a0bc31c7e6f7496be9052a2
Maju <maju@debian>
2021-10-16 20:04:25 +0200

    Initial commit

---
+++ src/main/java/pl/zbiczagromada/Magazynier/MagazynierApplication.java
@@ -0,0 +1,13 @@
+package pl.zbiczagromada.Magazynier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazynierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazynierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/zbiczagromada/Magazynier/MagazynierApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zbiczagromada.Magazynier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazynierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
